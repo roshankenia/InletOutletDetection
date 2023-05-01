17aaa048c1ec40c0b9c49d06ff5c4eebc4ec6fcb
Roshan Kenia <keniaroshan@gmail.com>
2023-05-01 17:49:26 -0400

    Update pebble_segmentation_util.py

--- pebble_segmentation_util.py
+++ pebble_segmentation_util.py
@@ -93,7 +93,7 @@ def pebble_segmentation(img, confidence=0.98):
     """
 
     # adjust brightness of image
-    img = adjust_contrast_brightness(img, contrast=1.0, brightness=75)
+    img = adjust_contrast_brightness(img, contrast=1.0, brightness=123)
     transform = VT.Compose([VT.ToTensor()])
     img = transform(img)
 
