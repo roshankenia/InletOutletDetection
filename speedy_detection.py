eddb9f8690279e63a6d94f5258bd60836f612b68
Roshan Kenia <keniaroshan@gmail.com>
2023-05-13 16:39:41 -0400

    Update speedy_detection.py

--- speedy_detection.py
+++ speedy_detection.py
@@ -235,7 +235,7 @@ def addToFrame(frame, video, frameNumber, videoTime, inletSavedPebbles=None):
 
 
 # create inlet video
-inletVideo = Video('S1060001In')
+inletVideo = Video('S1060001Out')
 
 # set frames count and fps
 num_frames = inletVideo.frame_count
