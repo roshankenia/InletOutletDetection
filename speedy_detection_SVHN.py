675c495488a74d1d8050c3c5a452fa1619977095
Roshan Kenia <keniaroshan@gmail.com>
2023-06-09 12:09:54 -0400

    Update speedy_detection_SVHN.py

--- speedy_detection_SVHN.py
+++ speedy_detection_SVHN.py
@@ -129,7 +129,7 @@ class Video():
                     for f in range(len(fixedImages)):
                         #downsize image
                         downsizedImage = fixedImages[f]
-                        scale_percent = 30 # percent of original size
+                        scale_percent = 20 # percent of original size
                         width = int(downsizedImage.shape[1] * scale_percent / 100) 
                         height = int(downsizedImage.shape[0] * scale_percent / 100) 
                         dim = (width, height) 
