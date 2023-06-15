8c31c6664e502f2933b0d6731c80a4a618a621f7
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2023-06-15 10:26:24 -0400

    Update carved_detection.py

--- carved_detection.py
+++ carved_detection.py
@@ -14,7 +14,7 @@ import math
 import time
 
 from speedy_orientation_util import segment_and_fix_image_range
-from speedy_detection_util import showbox_no_bottomY
+from speedy_detection_util_SVHN import showbox_no_bottomY
 from speedy_crop_util import digit_segmentation
 from speedy_pebble_util import updatePebbleLocation
 # ensure we are running on the correct gpu
@@ -40,14 +40,6 @@ for filename in filenames:
     img = cv2.imread(os.path.join(
         './Carved Pebbles/', filename))
 
-    # downsize image
-    scale_percent = 70  # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
-    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-
     # check if image has digits with confidence
     pebbleDigitsCrops, pebbleDigitBoxes, pebbleDigitScores, goodPredictions, goodMasks, originalDigitCrops = digit_segmentation(
         img)
@@ -60,11 +52,17 @@ for filename in filenames:
                 pebbleDigitsCrops[i], originalDigitCrops[i], 0.9)
             for f in range(len(fixedImages)):
                 # prediciton
-                predImg, predlabels, predScores = showbox_no_bottomY(
-                    fixedImages[f])
+                # downsize image
+                scale_percent = 25  # percent of original size
+                width = int(fixedImages[f].shape[1] * scale_percent / 100)
+                height = int(fixedImages[f].shape[0] * scale_percent / 100)
+                dim = (width, height)
+
+                downsizedImage = cv2.resize(fixedImages[f], dim, interpolation=cv2.INTER_AREA)
+                predImg, predlabels, predScores = showbox_no_bottomY(downsizedImage)
                 if predImg is not None:
                     cv2.imwrite(os.path.join(vis_tgt_path, str(i)+str(f)+filename), predImg)
                 else:
-                    cv2.putText(fixedImages[f], 'NONE', (5, 100), cv2.FONT_HERSHEY_SIMPLEX,
-                                4, (0, 0, 255), thickness=10)
-                    cv2.imwrite(os.path.join(vis_tgt_path, str(i)+str(f)+filename), fixedImages[f])
+                    cv2.putText(downsizedImage, 'NONE', (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                2, (0, 0, 255), thickness=10)
+                    cv2.imwrite(os.path.join(vis_tgt_path, str(i)+str(f)+filename), downsizedImage)
