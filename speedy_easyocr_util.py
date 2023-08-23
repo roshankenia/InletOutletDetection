21acdda3a1bf5969f6268f29f74995d93786d502
Roshan Kenia <keniaroshan@gmail.com>
2023-08-22 23:47:09 -0400

    downsize

--- speedy_easyocr_util.py
+++ speedy_easyocr_util.py
@@ -20,7 +20,7 @@ reader = easyocr.Reader(['en'])
 
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
+    # img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     img = clahe.apply(img)
     img = 255-img  # invert image. tesseract prefers black text on white background
