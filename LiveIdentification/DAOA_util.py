a3e4696c3d29df6b1900daff198d507a047cd19a
Roshan Kenia <keniaroshan@gmail.com>
2023-08-26 12:08:22 -0400

    Update DAOA_util.py

--- LiveIdentification/DAOA_util.py
+++ LiveIdentification/DAOA_util.py
@@ -238,10 +238,8 @@ def digit_area_orientation_alignment(img, og_img, confidence=0.9, rect_th=2, tex
                 cv2.putText(annImg, 'Angle:' + str(format(angle, '.2f')), (50, 75), cv2.FONT_HERSHEY_SIMPLEX,
                             3, (255, 255, 0), thickness=3)
                 # print('Horizontal not found')
-                cv2.line(annImg, tuple(map(round, barCenter)), tuple(
-                    map(round, picCenter)), (255, 255, 255), 10)
-                cv2.line(annImg, tuple(map(round, picCenter)), tuple(
-                    map(round, basePoint)), (255, 255, 255), 10)
+                cv2.line(annImg, (int(barCenter[0]), int(barCenter[1])), (int(picCenter[0]), int(picCenter[1])), (255, 255, 255), 10)
+                cv2.line(annImg, (int(picCenter[0]), int(picCenter[1])), (int(basePoint[0]), int(basePoint[1])), (255, 255, 255), 10)
                 rgb_mask = get_coloured_mask(mask, pred)
                 annImg = cv2.addWeighted(annImg, 1, rgb_mask, 0.5, 0)
                 cv2.rectangle(annImg, box[0], box[1],
